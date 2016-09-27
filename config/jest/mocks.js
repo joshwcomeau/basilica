0af5cf23e8fc635eb37a53a8154e3e006ff4ec02
Joshua Comeau <joshwcomeau@gmail.com>
2016-09-27 09:25:16 -0400

    Workaround for bug with Jest not handling React refs

--- config/jest/mocks.js
+++ config/jest/mocks.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-undef */
 jest.mock('react-dom');
+
+// NOTE: The MapBox integration I'm using uses refs, and React's test
+// renderer does not yet handle refs.
+//
+// This will be fixed soon (https://github.com/facebook/react/issues/7740)
+// Until then, I need to mock out the MapBox component
+jest.mock('react-mapbox-gl');
