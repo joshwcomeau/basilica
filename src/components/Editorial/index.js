1fdb2a16b4a563af5c53640c41ee723c9550eae9
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-07 13:20:45 -0400

    Start styling Editorial

--- src/components/Editorial/index.js
+++ src/components/Editorial/index.js
@@ -1,7 +1,10 @@
+/* eslint-disable max-len */
 // eslint-disable-next-line no-unused-vars
 import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
+import spotlightImg from '../../images/closeup-van-horne-cdn.jpg';
+import InnerWrapper from '../InnerWrapper';
 import './index.scss';
 
 
@@ -10,7 +13,15 @@ const Editorial = () => {
 
   return (
     <div className={classes}>
-      Words and Stuff
+      <InnerWrapper>
+        <img
+          className="editorial-image-spotlight"
+          src={spotlightImg}
+          alt="Spotlight on urban plans"
+        />
+        <h2>Authentic urban plans</h2>
+        <p>In September 2016, we procured a set of rare archival documents from the City of Montréal. These large documents are 4 feet long, and were used for decades—from the 60s through to the 90s—to help develop new policies and regulations. It is impossible to say how different the city would look today if not for these documents.</p>
+      </InnerWrapper>
     </div>
   );
 };
